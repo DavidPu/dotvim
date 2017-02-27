8906d755a9efc92b91e66179e323bd371eeb9780
David Pu <dpu@nvidia.com>
2017-02-27 10:32:26 -0800

    update find command

--- tags/gentags.sh
+++ tags/gentags.sh
@@ -1,6 +1,6 @@
 gentags() {
     files=tags.files
-    find $*  -type f ! -name ".*" ! -path "*.git/*" ! -path "*Documentation*" > $files
+    find "$@"  -type f ! -name ".*" ! -path "*.git/*" ! -path "*Documentation*" > $files
     gtags -f $files
     ctags -R --sort=yes --c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++  -L $files
 #    cscope -i$files
